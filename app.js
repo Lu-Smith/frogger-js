36e4a7f0a76eb47824a3610bb02cd79004e1f463
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2022-11-24 13:13:24 +0000

    Updated app.js

--- app.js
+++ app.js
@@ -9,28 +9,27 @@ const carsRight = document.querySelectorAll('.car-right');
 
 let currentIndex = 76;
 const width = 9;
+let TimerId;
 
 function moveFrog(e) {
-
     squares[currentIndex].classList.remove('frog');
-
     switch(e.key) {
         case 'ArrowLeft' :
             if (currentIndex % width !== 0)
                 currentIndex -= 1
-            break
+            break;
         case 'ArrowRight' :
             if (currentIndex % width < width - 1)
             currentIndex += 1
-            break
+            break;
         case 'ArrowUp' :
             if (currentIndex - width >= 0)
             currentIndex -= width
-            break
+            break;
          case 'ArrowDown' :
             if (currentIndex + width < width * width)
             currentIndex += width
-            break
+            break;
     }
     squares[currentIndex].classList.add('frog');
 }
@@ -38,40 +37,39 @@ function moveFrog(e) {
 document.addEventListener('keyup', moveFrog);
 
 function autoMoveElements() {
-    logsLeft.forEach(logLeft => moveLogLeft(logLeft))
-    logsRight.forEach(logRight => moveLogRight(logRight))
-    carsLeft.forEach(carLeft => moveCarLeft(carLeft))
-    carsRight.forEach(carRight => moveCarRight(carRight))
-}
+    logsLeft.forEach(logLeft => moveLogLeft(logLeft));
+    logsRight.forEach(logRight => moveLogRight(logRight));
+    carsLeft.forEach(carLeft => moveCarLeft(carLeft));
+    carsRight.forEach(carRight => moveCarRight(carRight));
+    lose();
+};
 
 function moveLogLeft(logLeft) {
-
     switch(true) {
        case logLeft.classList.contains('l1') :
          logLeft.classList.remove('l1')
          logLeft.classList.add('l2')
-         break
+         break;
        case logLeft.classList.contains('l2') :
           logLeft.classList.remove('l2')
           logLeft.classList.add('l3')
-          break 
+          break;
        case logLeft.classList.contains('l3') :
           logLeft.classList.remove('l3')
           logLeft.classList.add('l4')
-          break
+          break;
        case logLeft.classList.contains('l4') :
           logLeft.classList.remove('l4')
           logLeft.classList.add('l5')
-          break
+          break;
        case logLeft.classList.contains('l5') :
           logLeft.classList.remove('l5')
           logLeft.classList.add('l1')
-          break
+          break;
     }
 }
 
 function moveLogRight(logRight) {
-
     switch(true) {
        case logRight.classList.contains('l1') :
          logRight.classList.remove('l1')
@@ -94,10 +92,9 @@ function moveLogRight(logRight) {
           logRight.classList.add('l4')
           break
     }
-}
+};
 
 function moveCarLeft(carLeft) {
-
     switch(true) {
        case carLeft.classList.contains('c1') :
          carLeft.classList.remove('c1')
@@ -112,10 +109,9 @@ function moveCarLeft(carLeft) {
           carLeft.classList.add('c1')
           break
     }
-}
+};
 
 function moveCarRight(carRight) {
-
     switch(true) {
        case carRight.classList.contains('c1') :
          carRight.classList.remove('c1')
@@ -130,6 +126,15 @@ function moveCarRight(carRight) {
           carRight.classList.add('c2')
           break
     }
-}
+};;
+
+function lose() {
+    if (squares[currentIndex].classList.contains('c1')) {
+        resultDisplay.textContent = 'You lose!';
+        clearInterval(TimerId);
+        squares[currentIndex].classList.remove('frog');
+        document.removeEventListener('keyup', moveFrog);
+    }
+};
 
 setInterval(autoMoveElements, 1000);
