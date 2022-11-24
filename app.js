341b9a0d5b6eaa153910bff98f9863102510611d
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2022-11-24 10:44:38 +0000

    Updated app.js

---
+++ app.js
@@ -0,0 +1,12 @@
+const timeLeftDisplay = document.querySelector('#time-left');
+const resultDisplay = document.querySelector('#result');
+const startPauseDisplay = document.querySelector('#start-pause-button');
+const squres = document.querySelectorAll('.grid div');
+
+function moveFrog() {
+    console.log('moved')
+}
+
+document.addEventListener('keyup', moveFrog)
+
+
