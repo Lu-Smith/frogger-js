d0d48104fa74293f6d597a5bc672cdcf3dd043f7
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2022-11-24 10:49:54 +0000

    Updated app.js
    Created class frog

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const timeLeftDisplay = document.querySelector('#time-left');
 const resultDisplay = document.querySelector('#result');
 const startPauseDisplay = document.querySelector('#start-pause-button');
-const squres = document.querySelectorAll('.grid div');
+const squares = document.querySelectorAll('.grid div');
+
+let currentIndex = 0;
 
 function moveFrog() {
-    console.log('moved')
+    squares[currentIndex].classList.add('frog');
 }
 
 document.addEventListener('keyup', moveFrog)
-
 
